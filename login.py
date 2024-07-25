3092498ae795b0bcc90c7c73492655da3ddef7ff
Sage176520 <90260457+Sage176520@users.noreply.github.com>
2024-07-25 15:04:58 +0800

    Update login.py

--- login.py
+++ login.py
@@ -20,7 +20,7 @@ for i in range(acccounts):
     driver.set_window_size(1000, 720)
     driver.get("https://game.maj-soul.net/1/")
     print(f'Account {i+1} loading game...')
-    sleep(20)
+    sleep(60)
 
     #2.input email
     screen = driver.find_element(By.ID, 'layaCanvas')
@@ -45,6 +45,6 @@ for i in range(acccounts):
         .click()\
         .perform()
     print('Entering game...')
-    sleep(20) #loading...
+    sleep(60) #loading...
     print('Login success')
     driver.quit()
